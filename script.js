d9bf98ef1ee029037d5e1ecbc181bb3b7ae5ad57
afdon <afdonovan@alumni.princeton.edu>
2024-09-23 21:03:39 -0700

    plan js

---
+++ script.js
@@ -0,0 +1,17 @@
+// get HTML elements
+const board = document.getElementById('game-board');
+const instructionText = document.getElementById('instruction-text');
+const logo = document.getElementById('logo');
+const score = document.getElementById('score');
+const highScoreText = document.getElementById('highScore');
+
+// Define game variables
+// Draw map, snake, food
+// Draw snake
+// Create snake or food
+// Set pos of snake or food
+// Draw food func
+// Generate food
+// Move snake
+// Start game func
+// Listen to keypress
